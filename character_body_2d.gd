6622ee577dae00e12f995ccd6146c7c4808d267e
Felipe Alves <hwanleemulgogi@gmail.com>
2026-05-14 23:55:34 -0300

    Agora da pa joga tomate

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -1,38 +1,64 @@
 extends CharacterBody2D
-@onready var time = $AnimatedSprite2D/Timer
 
+@onready var time = $AnimatedSprite2D/Timer
+@export var tomato_scene: PackedScene
+@export var throw_interval := 0.6 
+var throw_cooldown := 0.0
 const SPEED = 150.0
 const JUMP_VELOCITY = -350.0
-
+var doing_action := false
+var facing_direction := 1
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+
 	if Input.is_action_just_pressed("Pulo") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 	if Input.is_action_just_released("Pulo") and velocity.y < 0 and not is_on_floor():
 		velocity.y *= 0
-	var direction := Input.get_axis("Direita","Esquerda")
-	if direction:
+
+	var direction := Input.get_axis("Direita", "Esquerda")
+	if direction != 0:
+		facing_direction = int(sign(direction))
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+
 	if velocity.x > 0:
 		$AnimatedSprite2D.flip_h = false
 	if velocity.x < 0:
 		$AnimatedSprite2D.flip_h = true
-	if velocity.x != 0 and velocity.y == 0:
-		$AnimatedSprite2D.play("Walk")
-		$AnimatedSprite2D.speed_scale = velocity.x / 50
-	else :
-		$AnimatedSprite2D.play("Idle")
-		$AnimatedSprite2D.speed_scale = 1
-	if velocity.y > 0:
-		$AnimatedSprite2D.play("Jump")
-	move_and_slide()
-	if Input.is_action_just_pressed("ui_accept"):
-		print("JOGAOTOMATEEE")
 
+	if not doing_action:
+		if velocity.y > 0:
+			$AnimatedSprite2D.play("Jump")
+		elif velocity.x != 0 and velocity.y == 0:
+			$AnimatedSprite2D.play("Walk")
+			$AnimatedSprite2D.speed_scale = velocity.x / 50
+		elif velocity.x == 0 and velocity.y == 0:
+			$AnimatedSprite2D.play("Idle")
+			$AnimatedSprite2D.speed_scale = 1
+
+	move_and_slide()
+
+	if throw_cooldown > 0.0:
+		throw_cooldown -= delta
+	if Input.is_action_pressed("ui_accept") and throw_cooldown <= 0.0 and not doing_action:
+		_throw_tomato()
+
+func _throw_tomato() -> void:
+	doing_action = true
+	$AnimatedSprite2D.speed_scale = 1
+	$AnimatedSprite2D.play("ThrowTomato")
+	var tomato = tomato_scene.instantiate() as RigidBody2D
+	var throw_point: Marker2D = $ThrowPoint
+	tomato.global_position = throw_point.global_position
+	var throw_dir := Vector2(facing_direction, -0.2).normalized()
+	get_tree().current_scene.add_child(tomato)
+	tomato.launch(throw_dir, velocity)
+	await $AnimatedSprite2D.animation_finished
+	doing_action = false
 
 func _on_timer_timeout() -> void:
-	pass # Replace with function body.
+	pass
--- rigid_body_2d.gd
+++ rigid_body_2d.gd
@@ -1,13 +1,48 @@
 extends RigidBody2D
 
+var speed := 200.0
+var trail: Line2D
+var max_trail_points := 40
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	linear_velocity.x = 200
-	linear_velocity.y = 100
-	pass # Replace with function body.
+	trail = Line2D.new()
+	trail.width = 6.0
+	trail.default_color = Color(1.0, 1.0, 1.0, 0.8)
+	trail.begin_cap_mode = Line2D.LINE_CAP_ROUND
+	trail.end_cap_mode = Line2D.LINE_CAP_ROUND
 
+	var gradient := Gradient.new()
+	gradient.set_color(1, Color(1.0, 0.769, 0.765, 0.663))
+	gradient.set_color(0, Color(0.645, 0.0, 0.233, 0.0))
+	trail.gradient = gradient
+	var curve := Curve.new()
+	curve.add_point(Vector2(0.0, 0.0))  # tail: width 0
+	curve.add_point(Vector2(1.0, 1.0))  # front: full width
+	trail.width_curve = curve
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+	get_tree().current_scene.add_child(trail)
+
+func launch(direction: Vector2, playerspeed: Variant) -> void:
+	linear_velocity = direction * speed + playerspeed
+	linear_velocity.y = -350
+
 func _process(delta: float) -> void:
-	pass
+	trail.add_point(global_position)
+	if trail.get_point_count() > max_trail_points:
+		trail.remove_point(0)
+
+func _on_body_shape_entered(body_rid: RID, body: Node, body_shape_index: int, local_shape_index: int) -> void:
+	$CollisionShape2D.set_deferred("disabled", true)
+	print("Hit object: ", body.name)
+	$".".freeze = true
+	$".".sleeping = true
+	$AnimatedSprite2D.play("Pop")
+	$AnimatedSprite2D/Timer.start(1)
+	$AnimationPlayer.play("Despawn")
+	await $AnimatedSprite2D/Timer.timeout
+	print("Despawn")
+	$".".queue_free()
+
+func _exit_tree() -> void:
+	if trail:
+		trail.queue_free()
