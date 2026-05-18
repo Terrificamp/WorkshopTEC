cb808fca722b2b45916f342bf27cb1c7e6426957
Paulo Junior <paulo.junior78888@gmail.com>
2026-05-18 20:49:40 -0300

    feat: adiciona os metodos _ready switch_boss

--- scripts/boss_manager.gd
+++ scripts/boss_manager.gd
@@ -1,16 +1,27 @@
 extends Node2D
 
-@onready var boss1 = $""
+@onready var boss1 = $"../Boss1"
+@onready var boss2 = $"../Boss2"
 
 
-var life = 20
-var phase = 1
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var life = 20#just a test
+var phase = 1# 1,2,3
 
+func _ready():
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	boss1.is_real = true
+	boss1.can_attack = true
+
+	boss2.is_real = false
+	boss2.can_attack = false
+
+func switch_boss(): #implement the animations
+
+	boss1.is_real = !boss1.is_real
+	boss2.is_real = !boss2.is_real
+
+	boss1.can_attack = !boss1.can_attack
+	boss2.can_attack = !boss2.can_attack
+
+	print("Trocaram!")
