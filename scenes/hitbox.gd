8aeb5f8deb4bf56ae840c6e2c7ff2e047c93cafe
Guilherme Camilo da Costa <camiloguilherme897@gmail.com>
2026-05-19 15:51:58 -0300

    Efeitos sonoros amais

--- scenes/hitbox.gd
+++ scenes/hitbox.gd
@@ -30,4 +30,6 @@ func _on_area_entered(area: Area2D) -> void:
 	if area.is_in_group("Boss"):
 		$AudioStreamPlayer2D.pitch_scale = randf_range(2.28,3.66)
 		$AudioStreamPlayer2D.play()
+		if Global.speciallevel <= 99:
+			Global.speciallevel += 2
 	pass # Replace with function body.
